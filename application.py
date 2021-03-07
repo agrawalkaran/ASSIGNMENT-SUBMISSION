36e8e7ac271dc8d773488229b9363a5395b8b472
agrawalkaran <46273058+agrawalkaran@users.noreply.github.com>
2021-03-07 17:46:40 +0530

    Update application.py

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ from datetime import datetime, date
 application = Flask(__name__)
 application.config.update(
     MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = '587',
+    MAIL_PORT = '25',
     MAIL_USE_SSL = True,
     MAIL_USE_TLS = False,
     MAIL_USERNAME = 'studentassignmentportal12@gmail.com',
